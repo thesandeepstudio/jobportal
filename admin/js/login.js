6af316ce31c5d535c015bbdbec9f0e47734fcdd3
Sandeep <thesandeep2080@gmail.com>
2026-02-14 10:39:28 +0545

    fix admin

--- admin/js/login.js
+++ admin/js/login.js
@@ -21,6 +21,7 @@ document
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
+        credentials: "include",
       });
 
       const data = await res.json();
